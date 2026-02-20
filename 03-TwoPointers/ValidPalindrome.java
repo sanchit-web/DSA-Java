687ad8502170be5249d7133056d124cd95854e9d
sanchit-web <sanchit221206@gmail.com>
2026-02-20 14:08:09 +0530

    Added ValidPalindrome

---
+++ 03-TwoPointers/ValidPalindrome.java
@@ -0,0 +1,35 @@
+ // Problem: Valid Palindrome
+// Platform: LeetCode
+// Pattern: Two Pointers
+// Difficulty: Easy
+// Time Complexity: O(n)
+// Space Complexity: O(1) 
+
+class Solution {
+    public boolean isPalindrome(String s) {
+        int i=0;
+        int j=s.length()-1;
+
+        while(i<j){
+            if(!(Character.isLetterOrDigit(s.charAt(i)))){
+                i++;
+                continue;
+            }
+            if(!(Character.isLetterOrDigit(s.charAt(j)))){
+                j--;
+                continue;
+            }
+
+            if(Character.toLowerCase(s.charAt(i))!=Character.toLowerCase(s.charAt(j))){
+                return false;
+            }
+            else{
+                i++;
+                j--;
+            }
+        }
+    return true;    
+    }
+}
+    
+
