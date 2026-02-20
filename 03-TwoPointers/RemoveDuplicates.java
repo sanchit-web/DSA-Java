ebde5021f5dcaa82d23d7bebdd69288e5d96cdd9
sanchit-web <sanchit221206@gmail.com>
2026-02-20 13:38:47 +0530

    Added Remove Dupicates

---
+++ 03-TwoPointers/RemoveDuplicates.java
@@ -0,0 +1,23 @@
+// Problem: Remove Duplicates
+// Platform: LeetCode
+// Pattern: Two Pointers
+// Difficulty: Easy
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int i=0;
+
+        for(int j=1;j<nums.length;j++){
+            if(nums[j]!=nums[j-1]){
+            i++;
+            nums[i]=nums[j];
+            
+            
+            }
+        }
+    return i+1;    
+    }
+}
+
