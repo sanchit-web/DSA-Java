212904c1ca7c29c1f83b564afa4d086135940693
sanchit-web <sanchit221206@gmail.com>
2026-02-24 12:38:29 +0530

    Added some medium Sliding Window problems

---
+++ 04-SlidingWindow/Medium/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,23 @@
+import java.util.HashSet;
+
+HashSet<Integer> set = new HashSet<>();
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int left=0;
+        int maxLen=0;
+        HashSet<Character> set=new HashSet<>();
+
+        for(int right=0;right<s.length();right++){
+            
+
+            while(set.contains(s.charAt(right))){
+                set.remove(s.charAt(left));
+                left++;
+            }
+
+            set.add(s.charAt(right));
+            maxLen=Math.max(maxLen,right-left+1);
+        }
+    return maxLen;    
+    }
+}
