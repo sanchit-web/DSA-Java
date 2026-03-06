785088e4db7a5c6fc8d8dd8658ad88b37aa8b2f3
sanchit-web <sanchit221206@gmail.com>
2026-03-06 22:52:02 +0530

    Added prefix sum medium

---
+++ 04-PrefixSum/Medium/Subarray Sums Divisible by K.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+class Solution {
+    public int subarraysDivByK(int[] nums, int k) {
+        HashMap<Integer , Integer> map=new HashMap<>();
+        map.put(0,1);
+        int prefixSum=0;
+        int count=0;
+
+        for(int num:nums){
+            prefixSum+=num;
+            int remainder=prefixSum % k;
+
+            if(remainder<0){
+                 remainder+=k;
+            }
+
+            if(map.containsKey(remainder)){
+                count+=map.get(remainder);
+                
+            }
+
+            map.put(remainder, map.getOrDefault(remainder,0)+1);
+        
+
+
+
+        }
+    return count;
+    }
+}
